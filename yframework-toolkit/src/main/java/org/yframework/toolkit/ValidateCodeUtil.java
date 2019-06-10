eea5025ea55327ea7f49d4f69d199ec0ba60e950
Jay-Y <570440569@qq.com>
2019-06-10 11:28:08 +0800

    验证码工具类完善

--- yframework-toolkit/src/main/java/org/yframework/toolkit/ValidateCodeUtil.java
+++ yframework-toolkit/src/main/java/org/yframework/toolkit/ValidateCodeUtil.java
@@ -10,7 +10,9 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Map;
 import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Description: ValidateCodeUtil.<br>
@@ -19,14 +21,21 @@ import java.util.Random;
 public enum ValidateCodeUtil
 {
     INSTANCE;
-    private final String _VALIDATE_CODE = "validateCode";
 
+    private static final String _KEY_R = "r";
+    private static final String _VALIDATE_CODE = "validateCode";
+    private final Map<String, Object> validateCodeCache = new ConcurrentHashMap<>();
     private int w = 70;
     private int h = 26;
 
     public boolean validate(HttpServletRequest request, String validateCode)
     {
         String code = (String) request.getSession().getAttribute(_VALIDATE_CODE);
+        if (StringUtil.isBlank(code) && request.getParameterMap().containsKey(_KEY_R))
+        {
+            String key = _VALIDATE_CODE + "-" + request.getParameter(_KEY_R);
+            code = this.getVCAfterAutoExpires(key);
+        }
         return validateCode.toUpperCase().equals(code);
     }
 
@@ -37,9 +46,9 @@ public enum ValidateCodeUtil
         response.setDateHeader("Expires", 0);
         response.setContentType("image/jpeg");
 
-		/*
+        /*
          * 得到参数高，宽，都为数字时，则使用设置高宽，否则使用默认值
-		 */
+         */
         String width = request.getParameter("width");
         String height = request.getParameter("height");
         if (StringUtils.isNumeric(width) && StringUtils.isNumeric(height))
@@ -51,15 +60,21 @@ public enum ValidateCodeUtil
         BufferedImage image = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
         Graphics g = image.getGraphics();
 
-		/*
+        /*
          * 生成背景
-		 */
+         */
         createBackground(g);
 
-		/*
+        /*
          * 生成字符
-		 */
+         */
         String s = createCharacter(g);
+        if (request.getParameterMap().containsKey(_KEY_R))
+        {
+            String key = _VALIDATE_CODE + "-" + request.getParameter(_KEY_R);
+            long expiresIn = 60000L;
+            this.setVCDataAndAutoExpires(key, s, expiresIn);
+        }
         request.getSession().setAttribute(_VALIDATE_CODE, s);
 
         g.dispose();
@@ -105,7 +120,7 @@ public enum ValidateCodeUtil
     private String createCharacter(Graphics g)
     {
         char[] codeSeq = {
-            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '2', '3', '4', '5', '6', '7', '8', '9'
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '2', '3', '4', '5', '6', '7', '8', '9'
         };
         String[] fontTypes = {"Arial", "Arial Black", "AvantGarde Bk BT", "Calibri"};
         Random random = new Random();
@@ -120,5 +135,41 @@ public enum ValidateCodeUtil
             s.append(r);
         }
         return s.toString();
+    }
+
+    private Object setVCDataAndAutoExpires(String preKey, Object data, long expiresIn)
+    {
+        this.validateCodeCache.put(preKey, data.toString());
+        this.validateCodeCache.put(preKey + "-" + data.toString(), System.currentTimeMillis());
+        this.validateCodeCache.put(preKey + "-" + data.toString() + "-expires_in", expiresIn);
+        return data;
+    }
+
+    private String getVCAfterAutoExpires(String preKey)
+    {
+        String data = (String) this.validateCodeCache.get(preKey);
+        if (null != data)
+        {
+            Long oldTime = (Long) this.validateCodeCache.get(preKey + "-" + data);
+            Long expiresIn = (Long) this.validateCodeCache.get(preKey + "-" + data + "-expires_in");
+            if (null != oldTime && null != expiresIn)
+            {
+                long remaining = System.currentTimeMillis() - oldTime;
+                if (remaining >= expiresIn)
+                {
+                    this.validateCodeCache.remove(preKey);
+                    this.validateCodeCache.remove(preKey + "-" + data);
+                    this.validateCodeCache.remove(preKey + "-" + data + "-expires_in");
+                    data = null;
+                }
+            }
+        }
+
+        return data;
+    }
+
+    private void clearVCCache()
+    {
+        this.validateCodeCache.clear();
     }
 }
